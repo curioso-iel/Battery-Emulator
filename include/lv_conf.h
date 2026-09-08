cdc99be615580fbfe5dd1c82490c61718701af10
casaantoes-eng <casaantoes@gmail.com>
2026-09-08 11:13:52 +0100

    Remove unused Montserrat font definitions

--- include/lv_conf.h
+++ include/lv_conf.h
@@ -30,29 +30,12 @@
 +#define LV_DRAW_SW_COMPLEX 1
 +
 +/* Fonts used by the reference UI. */
-/*Montserrat fonts with ASCII range and some symbols using bpp = 4
- *https://fonts.google.com/specimen/Montserrat*/
-#define LV_FONT_MONTSERRAT_8  0
-#define LV_FONT_MONTSERRAT_10 0
-#define LV_FONT_MONTSERRAT_12 0
-#define LV_FONT_MONTSERRAT_14 1
-#define LV_FONT_MONTSERRAT_16 0
-#define LV_FONT_MONTSERRAT_18 0
-#define LV_FONT_MONTSERRAT_20 1
-#define LV_FONT_MONTSERRAT_22 0
-#define LV_FONT_MONTSERRAT_24 1
-#define LV_FONT_MONTSERRAT_26 0
-#define LV_FONT_MONTSERRAT_28 1
-#define LV_FONT_MONTSERRAT_30 0
-#define LV_FONT_MONTSERRAT_32 0
-#define LV_FONT_MONTSERRAT_34 0
-#define LV_FONT_MONTSERRAT_36 1
-#define LV_FONT_MONTSERRAT_38 0
-#define LV_FONT_MONTSERRAT_40 1
-#define LV_FONT_MONTSERRAT_42 0
-#define LV_FONT_MONTSERRAT_44 0
-#define LV_FONT_MONTSERRAT_46 0
-#define LV_FONT_MONTSERRAT_48 0
++#define LV_FONT_MONTSERRAT_14 1
++#define LV_FONT_MONTSERRAT_20 1
++#define LV_FONT_MONTSERRAT_24 1
++#define LV_FONT_MONTSERRAT_36 1
++#define LV_FONT_MONTSERRAT_40 1
++#define LV_FONT_DEFAULT &lv_font_montserrat_14
 +
 +#define LV_USE_LABEL 1
 +#define LV_USE_CHART 1
@@ -62,3 +45,4 @@
 +#define LV_BUILD_EXAMPLES 0
 +
 +#endif
+
